8c973686936fba36c7da93135353c829262bad5b
유찬홍 <chanhongy6@naver.com>
2022-06-07 16:47:28 +0900

    체크박스 만들기

--- 체크버튼.py
+++ 체크버튼.py
@@ -10,10 +10,13 @@ chkbox.select() #자동 선택처리
 #chkbox.deselect() # 선택 해제처리
 chkbox.pack()
 
-
+chkvar2 = IntVar()
+chkbox2 = Checkbutton(root, text="걍 안보기", variable=chkvar2)
+chkbox2.pack()
 
 def btncmd():
     print(chkvar.get()) # 0일때는 체크 해제, 1일때는 체크 
+    print(chkvar2.get()) # 얘도 똑같음
 
 btn = Button(root, text="클릭", command=btncmd)
 btn.pack()
